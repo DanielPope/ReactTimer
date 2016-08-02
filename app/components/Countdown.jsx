36338c1a871802aa48a5cfbecad788a35cc9c230
Daniel Pope <dpope@dminc.com>
2016-08-02 15:50:04 -0400

    add navigation

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var Countdown = (props) => {
+  return (
+    <div>
+      <div>
+          <p> Countdown.jsx rendered</p>
+      </div>
+    </div>
+  )
+}
+module.exports = Countdown;
---
+++ app/components/Timer.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var Timer = (props) => {
+  return (
+    <div>
+      <div>
+          <p> Timer.jsx rendered</p>
+      </div>
+    </div>
+  )
+}
+module.exports = Timer;
